072fa009287c97df2481fd6afa0387c2e47696c2
Nir Shihor <nirshihor@gmail.com>
2021-01-27 16:01:26 +0000

    g

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ const app = express();
 const path = require("path");
 // const ejs = require("ejs");
 
+app.set("view engine", "html");
+
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
 
-app.get("/", function () {
-  res.sendFile(path.join(__dirname + "index.html"));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.sendFile("index.html");
 });
 
 app.listen(port, function () {
