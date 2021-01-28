e9593ed3a6c20bfdd8c320756c9a5d35d7c77318
Nir Shihor <nirshihor@gmail.com>
2021-01-28 20:49:14 +0000

    switched to ejs

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-// const ejs = require("ejs");
+const ejs = require("ejs");
 
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
 
+app.set("view-engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.sendFile("index.html");
+  res.render("index.ejs");
 });
 
 app.listen(port, function () {
