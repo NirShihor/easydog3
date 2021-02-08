b94417d037c50fbd7e9d7d3747ed08832e96e0fd
Nir Shihor <nirshihor@gmail.com>
2021-02-08 21:53:44 +0000

    connect flash sorted

--- app.js
+++ app.js
@@ -1,39 +1,57 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const session = require("express-session");
 const path = require("path");
 const ejs = require("ejs");
+var expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
+const flash = require("connect-flash");
+const mail = require("./mail"); //use mail.js file - sending mail through contact fortm
 
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
 
-app.set("view-engine", "ejs");
+mailSend = mail.sendMail; //Function parameters in mail.js from form (contact.ejs) for post request below
+
+app.use(mail.mailSession); //This passes the session from mail.js as without it throws an error re flash requiring sessions
+app.use(flash());
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(expressLayouts);
 app.use(express.static("public"));
+sendAMessage = mail.sendNewMessage;
+
+app.set("views", path.join(__dirname, "views")); //This should maybe be deleted
+app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-  res.render("index.ejs");
+  res.render("index");
 });
 
 app.get("/about", function (req, res) {
-  res.render("about.ejs");
+  res.render("about");
 });
 
 app.get("/whyEasyDog", function (req, res) {
-  res.render("whyEasyDog.ejs");
+  res.render("whyEasyDog");
 });
 
 app.get("/testimonials", function (req, res) {
-  res.render("testimonials.ejs");
+  res.render("testimonials");
 });
 
 app.get("/services", function (req, res) {
-  res.render("services.ejs");
+  res.render("services");
 });
 
 app.get("/contact", function (req, res) {
-  res.render("contact.ejs");
+  res.render("contact");
+  req.flash("success_msg");
 });
+
+app.post("/send", mailSend);
 
 app.listen(port, function () {
   console.log("Server started successfully");
---
+++ mail.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const app = express();
+const session = require("express-session");
+const flash = require("connect-flash");
+const bodyParser = require("body-parser"); //Once everything is working check if required here
+const DOMAIN = "easydog.co.uk";
+
+const api_key = process.env.API_KEY;
+
+// For Mailgun
+const mailgun = require("mailgun-js");
+
+const mg = mailgun({
+  apiKey: api_key,
+  domain: DOMAIN,
+  host: "api.eu.mailgun.net", //this is required when in Europe or else will not send and will get a 'forbidden' error 401
+});
+
+const mailSession = app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+exports.mailSession = mailSession;
+
+app.use(flash());
+
+// function sendMessage() {
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  next();
+});
+// }
+
+// exports.sendNewMessage = sendMessage;
+
+app.use(bodyParser.urlencoded({ extended: false })); //Once everything is working check if required here
+
+// Creater a function here that can be passed to post request in app.js
+function sendMail(req, res) {
+  const data = {
+    name: req.body.name,
+    from: req.body.email,
+    subject: "A message from your website",
+    to: "info@easydog.co.uk",
+    text: req.body.message,
+  };
+  console.log("and here");
+
+  mg.messages().send(data, function (err, body) {
+    if (err) {
+      console.log(err);
+    }
+    console.log(body);
+  });
+
+  console.log(data);
+  req.flash(
+    "success_msg",
+    "Your message has been received and a reply will be sent shortly."
+  );
+
+  console.log("and also here");
+
+  res.redirect("contact");
+}
+
+exports.sendMail = sendMail;
+
+console.log("here");
