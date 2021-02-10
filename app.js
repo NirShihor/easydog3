4ac457c69a0e92e66db8122267c5e0923c200064
Nir Shihor <nirshihor@gmail.com>
2021-02-10 11:14:07 +0000

    changes to scripts

--- app.js
+++ app.js
@@ -53,10 +53,6 @@ app.get("/contact", function (req, res) {
 
 app.post("/send", mailSend);
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log(
-    "Express server listening on port %d in %s mode",
-    this.address().port,
-    app.settings.env
-  );
+app.listen(port, function () {
+  console.log("Server started successfully");
 });
